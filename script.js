9ef7d7ee7c292436679531e7a1b26232192a4f41
Christos Patrinopoulos <christpa98@windowslive.com>
2023-10-17 23:15:04 +0300

    Session 2: button added, grid rebuild functionality

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-function create(){
+function create(size){
 
     const container = document.createElement('div');
     container.setAttribute('class', 'container');
     // container.setAttribute('style', 'display: flex; flex-direction: column; justify-content: space-around; align-items: center; width: 100%; height: 100%;');
     document.body.appendChild(container);
 
-    for (let i=0; i<4; i++) {
+    for (let i=0; i<size; i++) {
         //const row = [];
         let row = document.createElement('div');
         row.setAttribute('class', 'row');
         // row.setAttribute('style', 'display: flex; flex-direction: row; justify-content: space-around; align-items: center;');
         document.querySelector('.container').appendChild(row);
-        for (let j=0; j<4; j++) {
+        for (let j=0; j<size; j++) {
             const cell = document.createElement('div');
             cell.setAttribute('class', 'cell');
             // cell.setAttribute('style', 'wdith: 30px; height: 30px; border-width: 3px; border-style: solid;');
-            cell.innerText = "Hello World";
+            // cell.innerText = "Hello World";
 
             cell.addEventListener('mouseover', function() {
                 cell.setAttribute('style', 'background-color: grey');
             });
-            cell.addEventListener('mouseout', function() {
-                cell.setAttribute('style', 'background-color: white');
-            });
+            // cell.addEventListener('mouseout', function() {
+            //     cell.setAttribute('style', 'background-color: white');
+            // });
             
 
             document.querySelector('.container').lastChild.appendChild(cell);
@@ -30,6 +30,16 @@ function create(){
     }
 
     // alert('Hello World');
+}
+
+function setSize() {
+    // Remove the current grid
+    let container = document.getElementsByClassName('container');
+    if (container.length != 0) document.body.removeChild(container[0]);
+
+    // Set the new grid
+    let size = prompt('Enter the size of the grid: ');
+    create(size);
 }
 
 // create();
